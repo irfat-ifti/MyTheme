b745e8ac9876b4f920e5605aae78fbd466a4fa8b
irfat-ifti <irfatuddinifti@gmail.com>
2025-07-22 12:52:56 +0600

    add customizer_register for header section

--- functions.php
+++ functions.php
@@ -23,3 +23,25 @@ function mt_css_js_file_calling(){
     wp_enqueue_script("main");
 }
 add_action("wp_enqueue_scripts","mt_css_js_file_calling");
+
+// Add Header customize options
+
+function mt_customizer_register($wp_customize){
+    $wp_customize->add_section("mt_header_area", array(
+        "title" => __("Header Area", "irfatifti"),
+        "description" => "If you want to customize your your header area! You can do it from here.",
+    ));
+
+    $wp_customize->add_setting("mt_logo", array(
+        "default" => get_bloginfo("template_directory") . "/images/logo.png",
+
+    ));
+
+    $wp_customize->add_control(new Wp_Customize_Image_Control($wp_customize,"mt_logo", array(
+        "label" => "Upload Image",
+        "section" => "mt_header_area",
+        "setting" => "mt_logo",
+    )));
+}
+
+add_action("customize_register","mt_customizer_register");
--- index.php
+++ index.php
@@ -15,7 +15,16 @@
 </head>
 
 <body <?php body_class()?>>
-
+    <div class="header-area">
+        <div class="container">
+            <div class="row d-flex justify-content-between">
+                <div class="col-md-1">
+                    <a href="#"><img src="<?php echo get_theme_mod("mt_logo");?>" alt=""></a>
+                </div>
+                <div class="col-md-9"></div>
+            </div>
+        </div>
+    </div>
 
     <?php wp_footer()?>
 </body>
